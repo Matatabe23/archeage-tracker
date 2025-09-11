59131d46d8ccf939cfadbf51ad485dcfe1845a51
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2025-09-12 01:46:30 +0300

    Добавил описание для операции регистрации нового пользователя в декоратор CreateUserDoc.

--- server/src/module/endpoints/users/decorators/create-user.decorator.ts
+++ server/src/module/endpoints/users/decorators/create-user.decorator.ts
@@ -1,9 +1,15 @@
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponse, ApiOperation } from '@nestjs/swagger';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { ErrorDto } from 'src/dto/error.dto';
 
 export function CreateUserDoc(): MethodDecorator {
 	return function (target, propertyKey, descriptor) {
+		// Добавляем описание ручки
+		ApiOperation({
+			summary: 'Регистрация нового пользователя',
+			description: 'Создаёт нового пользователя и отправляет письмо для подтверждения email'
+		})(target, propertyKey, descriptor);
+
 		ApiResponse({
 			status: 201,
 			description: 'Пользователь успешно зарегистрирован',
