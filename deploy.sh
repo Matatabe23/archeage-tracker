70cbea670a1dd66556cf9f0ea95f7ae1f1404607
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2025-09-21 02:51:28 +0300

    Refactored deploy script to streamline deployment process by removing logging and SSH workflow. Updated to use environment variables for configuration and simplified container build and startup commands.

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,19 @@
-APP_DIR="/home/prod"
-LOG_DIR="$APP_DIR/data/deploy"
-LOG_FILE="$LOG_DIR/deploy.log"
-NGINX_TEMPLATE="$APP_DIR/nginx.conf.template"
-NGINX_CONFIG="$APP_DIR/nginx.conf"
-
-mkdir -p "$LOG_DIR"
-
-cd "$APP_DIR" || exit 1
-
-echo "=== Деплой начат: $(date)" >> "$LOG_FILE" 2>&1
-
-echo "➤ Сброс и стягивание последней версии..." | tee -a "$LOG_FILE"
-git pull >> "$LOG_FILE" 2>&1
-
-echo "➤ Генерация nginx.conf из шаблона..." | tee -a "$LOG_FILE"
-
 # Загрузка переменных из .env и генерация nginx.conf
 set -o allexport
 source <(grep -v '^#' .env | tr -d '\r')
 set +o allexport
 
+APP_DIR="/var/www/${TYPE_PROJECT}"
+NGINX_TEMPLATE="$APP_DIR/nginx.conf.template"
+NGINX_CONFIG="$APP_DIR/nginx.conf"
+
+mkdir -p "$APP_DIR/data/deploy"
+
+cd "$APP_DIR" || exit 1
+
+git pull origin ${BRANCH_DEPLOY}
+
 envsubst '${MY_DOMAIN}' < "$NGINX_TEMPLATE" > "$NGINX_CONFIG"
-echo "nginx.conf сгенерирован успешно." | tee -a "$LOG_FILE"
 
-echo "➤ Пересборка контейнеров..." | tee -a "$LOG_FILE"
-docker compose build --no-cache >> "$LOG_FILE" 2>&1
-docker compose up -d >> "$LOG_FILE" 2>&1
-
-echo "✅ Деплой завершён: $(date)" >> "$LOG_FILE" 2>&1
+docker compose build --no-cache
+docker compose up -d
