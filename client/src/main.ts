d510a30dce630e3662d734110b812e0ebc49de28
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2025-09-23 00:43:55 +0300

    Refactor main.ts to remove socket connection from global properties and improve code readability. Update package.json and package-lock.json to include nodemailer dependency for email functionality.

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,22 +10,18 @@ import { router } from './app/router';
 
 import vuetify from '@/shared/plugins/vuetify';
 import Toast, { toastOptions } from '@/shared/plugins/toast';
-import { socket } from '@/shared';
+
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.config.globalProperties.$socket = socket;
-
 app.use(FloatingVue)
-	.use(pinia)
-	.use(router)
-	.use(VueLazyload, {
-		preLoad: 2
-	})
-	.use(Toast, toastOptions)
-	.use(vuetify)
-	.use(VueTheMask)
-	.mount('#app');
-
-socket.connect();
+    .use(pinia)
+    .use(router)
+    .use(VueLazyload, {
+        preLoad: 2
+    })
+    .use(Toast, toastOptions)
+    .use(vuetify)
+    .use(VueTheMask)
+    .mount('#app');
