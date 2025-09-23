2d84e6c950e3bfc0bc3fa03e44dbcfc18c298e63
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2025-09-24 00:21:33 +0300

    Refactor CharactersController by removing roles guard and associated permissions, simplifying access control for character endpoints.

--- server/src/module/endpoints/characters/characters.controller.ts
+++ server/src/module/endpoints/characters/characters.controller.ts
@@ -16,9 +16,6 @@ import { CreateCharacterDto } from './dto/create-character.dto';
 import { UpdateCharacterDto } from './dto/update-character.dto';
 import { GetCharactersDto } from './dto/get-characters.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { RolesGuard } from 'src/guards/roles.guard';
-import { Roles } from 'src/decorators/roles.decorator';
-import { ECharacterPermission } from 'src/types/permissions/characters';
 import { CreateCharacterDoc } from './decorators/create-character.decorator';
 import { UpdateCharacterDoc } from './decorators/update-character.decorator';
 import { DeleteCharacterDoc } from './decorators/delete-character.decorator';
@@ -28,7 +25,7 @@ import { GetCharacterByIdDoc } from './decorators/get-character-by-id.decorator'
 @Controller('characters')
 @ApiTags('Персонажи')
 @ApiBearerAuth('access-token')
-@UseGuards(AuthGuard, RolesGuard)
+@UseGuards(AuthGuard)
 export class CharactersController {
 	constructor(private readonly charactersService: CharactersService) {}
 
@@ -52,7 +49,6 @@ export class CharactersController {
 
 	@Get()
 	@GetCharactersDoc()
-	@Roles(ECharacterPermission.GET_CHARACTERS)
 	async list(@Query() query: GetCharactersDto) {
 		return this.charactersService.list(query);
 	}
